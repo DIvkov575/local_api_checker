ee49bb38d49df4a30af4c362fada771e72b6a825
Divkov575 <dmitriy@ivkov.net>
2024-02-24 11:24:10 -0500

    tweaks to logging - simplify logger config + docs + remove result type from handle_stream

--- src/logging.rs
+++ src/logging.rs
@@ -1,10 +1,14 @@
 use std::error::Error;
-use std::io::Read;
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 use std::time::SystemTime;
 use log::info;
 
 pub fn logging(port: usize, name: &str) -> Result<(), Box<dyn Error>> {
+    /// logging command
+    /// port - localhost port
+    /// name - file path to store logs
+
     setup_logger()?;
     let addr = format!("127.0.0.1:{}", port);
     let listener = TcpListener::bind(addr)?;
@@ -12,26 +16,32 @@ pub fn logging(port: usize, name: &str) -> Result<(), Box<dyn Error>> {
     println!("Listener Started");
     info!("Logger Started");
 
-    for stream in listener.incoming() {
-        let mut buf: String = "Default".to_string();
-        let content = stream?.read_to_string(&mut buf)?;
-        info!("connection");
-    }
+    listener.incoming().for_each(|x| handle_stream(x.unwrap()));
+
     Ok(())
 }
 
+
+fn handle_stream(mut stream: TcpStream) {
+    /// Function which executes for each new stream connection
+    /// Logs to stdout + file and responds to tcp request with http OK
+
+    info!("connection");
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
 fn setup_logger() -> Result<(), fern::InitError> {
+    /// create logger connection
     fern::Dispatch::new()
         .format(|out, message, record| {
             out.finish(format_args!(
-                "[{} {} {}] {}",
+                "[{} {}] {}",
                 humantime::format_rfc3339_seconds(SystemTime::now()),
                 record.level(),
-                record.target(),
                 message
             ))
         })
-        .level(log::LevelFilter::Debug)
         .chain(std::io::stdout())
         .chain(fern::log_file("output.log")?)
         .apply()?;
